fac4808e819af8d26b8a114ee423680e5a57940f
makedon2758 <121632492+makedon2758@users.noreply.github.com>
2023-07-15 18:30:30 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -22,6 +22,47 @@
             }));
         }
     }), 0);
+    const script_link = document.querySelectorAll("a");
+    script_link.forEach((el => {
+        el.addEventListener("click", (e => {
+            e.preventDefault();
+        }));
+    }));
+    const modalOpen = document.querySelector(".balans-heder__menu");
+    const modal = document.querySelector(".modal");
+    const modalClose = document.querySelector(".titl__right");
+    document.querySelector(".button__deposites");
+    document.querySelector(".button__conclusion");
+    const modalReffOpen = document.querySelector(".header-page__reff");
+    const modalReffClose = document.querySelector(".mpdal__reff-close");
+    const modalReff = document.querySelector(".modal__reff");
+    function modalHidden(modal) {
+        modal.classList.add("hidden");
+    }
+    modalHidden(modal);
+    modalHidden(modalReff);
+    function openModal(button, modal) {
+        button.addEventListener("click", (() => {
+            modal.classList.add("show");
+            modal.classList.remove("hidden");
+        }));
+    }
+    openModal(modalOpen, modal);
+    openModal(modalReffOpen, modalReff);
+    function closeModal(button, modal) {
+        button.addEventListener("click", (() => {
+            modal.classList.remove("show");
+            modal.classList.add("hidden");
+        }));
+        modal.addEventListener("click", (e => {
+            if (e.target === modal) {
+                modal.classList.remove("show");
+                modal.classList.add("hidden");
+            }
+        }));
+    }
+    closeModal(modalClose, modal);
+    closeModal(modalReffClose, modalReff);
     window["FLS"] = true;
     isWebp();
 })();
